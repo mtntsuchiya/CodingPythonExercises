a3d78d79ac2014108cffdc72678dc1989dc6ac5e
mtntsuchiya <mirian@nzp-md25jf07sdhjt.us.sinet.si.edu>
2016-07-18 14:49:29 -0400

    final
    final

--- 2_homework.py
+++ 2_homework.py
@@ -16,48 +16,22 @@ while True:
 	choice = input("Please select the desired operation: ")
 	print("")
 	if choice not in options:
-		print("")
 		print("Please choose a valid choice")
-		continue
-	else:
-		if choice == "1"
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-while True:
-	choice = input("Please choose the desired operation: ")
-	if choice not in options:
 		print("")
-		print("Please choose a valid option.")
 		continue
-	else:
+	elif choice == "q":
+			print("Thanks for using the calculator!")
+			print("")
+			quit()
+	elif choice == "1":
 		while True:
 			try:
 				x = int(input("Please enter the first number: "))
 			except ValueError:
 				print("Your input is invalid.")
 				continue
-		else:
-			break
+			else:
+				break
 		while True:
 			try:
 				y = int(input("Please enter the second number: "))
@@ -66,64 +40,77 @@ while True:
 				continue
 			else:
 				break
-
-		if choice == "1":
-			print("")
-			print("The sum of", x," and ",y," is ",(x+y))
-			print("")
-			break
-		elif choice == "2":
-			print("")
-			print("The subtraction of", x," and ",y," is ",(x-y))
-			print("")
-			break
-		elif choice == "3":
-			print("")
-			print("The multiplication of", x," and ",y," is ",(x*y))
-			print("")
-			break
-		elif choice == "4":
-			if y != 0:
-				print("")
-				print("The division of", x," by ",y," is ",(x/y))
-				print("")
-				break
+		print("")
+		print("The sum of ", x, "and", y, "is", (x+y))
+		print("")
+		quit()
+	elif choice == "2":
+		while True:
+			try:
+				x = int(input("Please enter the first number: "))
+			except ValueError:
+				print("Your input is invalid.")
+				continue
 			else:
-				while True:
-					try:
-						z = int(input("Please enter a valid non zero number: "))
-						if z != 0:
-							print("")
-							print("The division of", x, "by", z, "is" , (x/z))
-							print("")
-							break
-						else:
-							print("NON-ZERO!!")
-							continue
-					except ValueError:
-						print("Your input is invalid.")
-						continue
-					else:
-						break
-		elif choice == "q":
-			print("")
-			print("Thanks for using the calculator")
-			print("")
-			quit()
-
-# while True:
-# 	try:
-# 		x = int(input("Please enter the first number: "))
-# 	except ValueError:
-# 		print("Your input is invalid.")
-# 		continue
-# 	else:
-# 		break
-# while True:
-# 	try:
-# 		y = int(input("Please enter the second number: "))
-# 	except ValueError:
-# 		print("Your input is invalid.")
-# 		continue
-# 	else:
-# 		break
+				break
+		while True:
+			try:
+				y = int(input("Please enter the second number: "))
+			except ValueError:
+				print("Your input is invalid.")
+				continue
+			else:
+				break
+		print("")
+		print("The quocient of ", x, "and", y, "is", (x-y))
+		print("")
+		quit()
+	elif choice == "3":
+		while True:
+			try:
+				x = int(input("Please enter the first number: "))
+			except ValueError:
+				print("Your input is invalid.")
+				continue
+			else:
+				break
+		while True:
+			try:
+				y = int(input("Please enter the second number: "))
+			except ValueError:
+				print("Your input is invalid.")
+				continue
+			else:
+				break
+		print("")
+		print("The product of ", x, "and", y, "is", (x*y))
+		print("")		
+		quit()
+	elif choice == "4":
+		while True:
+			try:
+				x = int(input("Please enter the first number: "))
+			except ValueError:
+				print("Your input is invalid.")
+				continue
+			else:
+				break
+		while True:
+			try:
+				y = int(input("Please enter a number different from zero: "))
+				if y == 0:
+					print("")
+					print("NON-ZERO!!")
+					print("")
+					continue
+				else:
+					break	
+			except ValueError:
+				print("Your input is invalid.")
+				continue
+			else:
+				break
+		print("")
+		print("The division of ", x, "and", y, "is", (x/y))
+		print("")
+		break
